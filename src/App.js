06f1380cbf79c2e0f7d23afefae0c660847bcf60
Rgarg6272 <73298337+Rgarg6272@users.noreply.github.com>
2023-07-11 17:08:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import ResultsTable from "./components/Header/ResultsTable/ResultsTable";
+import UserInput from "./components/Header/UserInput/UserInput";
+
+function App() {
+  const [userInput, setUserInput] = useState(null);
+
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+  };
+
+  const yearlyData = [];
+
+  if (userInput) {
+    let currentSavings = +userInput["current-savings"]; // feel free to change the shape of this input object!
+    const yearlyContribution = +userInput["yearly-contribution"]; // as mentioned: feel free to change the shape...
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
+
+    // The below code calculates yearly results (total savings, interest etc)
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        // feel free to change the shape of the data pushed to the array!
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  }
+
+  return (
+    <div>
+      <Header />
+      <UserInput onCalculate={calculateHandler} />
+
+      {userInput == null ? (
+        <p style={{ display: "flex", justifyContent: "center" }}>
+          No Investement Calculated Yet
+        </p>
+      ) : (
+        <ResultsTable
+          data={yearlyData}
+          initialInvestment={userInput["current-savings"]}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
